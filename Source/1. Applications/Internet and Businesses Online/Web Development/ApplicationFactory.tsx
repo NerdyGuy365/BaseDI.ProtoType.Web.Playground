0e5f0216bac2610c97728604cd0802075b6ab6f6
Mark Richardson <BusinessGuy1979@gmail.com>
2018-12-08 13:17:38 -0600

    Working version of structure
    Working version of structure

---
+++ Source/1. Applications/Internet and Businesses Online/Web Development/ApplicationFactory.tsx
@@ -0,0 +1,21 @@
+﻿//IMPORT APPLICATION COMPONENTS
+import { Global, IGlobalContainerProps, IGlobalHeaderProps, IGlobalBodyProps, IGlobalFooterProps } from '../../../Global';
+import { default as Application_Internet_And_Businesses_Online_Blogging_Test_BlogApplication_1_0 } from '../Blogging/Application_Internet_And_Businesses_Online-Blogging_Test_BlogApplication_1_0';
+import { default as Application_Internet_And_Businesses_Online_Site_Promotion_Test_BlogApplication_Menu_1_0 } from '../Site Promotion/Application_Internet_And_Businesses_Online-Site_Promotion_Test_BlogApplication_Menu_1_0';
+
+//DECLARE FACTORY METHOD
+const Application = (props: IGlobalContainerProps): any => {
+    switch (props.ApplicationName != undefined && props.ApplicationName != null && props.ApplicationName.toLowerCase())
+    {
+        case "application_internet_and_businesses_online-blogging_test_blogapplication_1_0":
+            return new Application_Internet_And_Businesses_Online_Blogging_Test_BlogApplication_1_0(props);
+        case "application_internet_and_businesses_online-site_promotion_test_blogapplication_menu_1_0":
+            return Application_Internet_And_Businesses_Online_Site_Promotion_Test_BlogApplication_Menu_1_0(props);
+    }
+}
+
+const ApplicationFactory = (props: IGlobalContainerProps): any => {
+    return Application(props)
+}
+
+export default ApplicationFactory;
---
+++ Source/4. Pieces/Internet and Businesses Online/Web Development/PieceFactory.tsx
@@ -0,0 +1,16 @@
+﻿//IMPORT APPLICATION COMPONENTS
+import { Global, IGlobalContainerProps, IGlobalHeaderProps, IGlobalBodyProps, IGlobalFooterProps } from '../../../Global';
+
+//DECLARE FACTORY METHOD
+const Piece = (props: IGlobalContainerProps): any => {
+    //switch (props.RenderID != undefined && props.RenderID != null && props.RenderID.toLowerCase()) {
+    //    case "piece_internet_and_businesses_online-site_promotion_test_navigation_menu_1_0":
+    //        return Piece_Internet_And_Businesses_Online_Site_Promotion_Test_Navigation_Menu_1_0(props);
+    //}
+}
+
+const PieceFactory = (props: IGlobalContainerProps): any => {
+    return Piece(props)
+}
+
+export default PieceFactory;
