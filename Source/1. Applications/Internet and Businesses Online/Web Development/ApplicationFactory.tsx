1ae5afe296faf9de2de6cccfcfc5125cb0ce711d
Richardson, Mark <BusinessGuy1979@gmail.com>
2018-12-13 15:49:15 -0600

    Reuploading
    Improved on BaseDI standards

---
+++ Source/1. Applications/Internet and Businesses Online/Web Development/ApplicationFactory.tsx
@@ -0,0 +1,29 @@
+﻿//IMPORT APPLICATION COMPONENTS
+import { Global, IGlobalContainerProps, IGlobalHeaderProps, IGlobalBodyProps, IGlobalFooterProps } from '../../../Global';
+import { default as Application_Internet_And_Businesses_Online_Demo_Blog_Blogging_1_0 } from '../Blogging/Application – Internet and Businesses Online – Demo Blog, Blogging - 1_0';
+import { default as Application_Internet_And_Businesses_Online_Demo_Blog_Web_Design_1_0 } from '../Web Design/Application – Internet and Businesses Online – Demo Blog, Web Design - 1_0';
+
+//DECLARE FACTORY ENUM
+enum ApplicationNameEnum {
+    Application_Internet_And_Businesses_Online_Demo_Blog_Blogging_1_0 = "application - internet and businesses online - demo blog, blogging - 1.0",
+    Application_Internet_And_Businesses_Online_Demo_Blog_Web_Design_1_0 = "application - internet and businesses online - demo blog, web design - 1.0"
+}
+
+export { ApplicationNameEnum }
+
+//DECLARE FACTORY METHOD
+const Application = (props: IGlobalContainerProps): any => {
+    switch (props.ApplicationName != undefined && props.ApplicationName != null && props.ApplicationName.toLowerCase())
+    {
+        case ApplicationNameEnum.Application_Internet_And_Businesses_Online_Demo_Blog_Blogging_1_0:
+            return new Application_Internet_And_Businesses_Online_Demo_Blog_Blogging_1_0(props);
+        case ApplicationNameEnum.Application_Internet_And_Businesses_Online_Demo_Blog_Web_Design_1_0:
+            return Application_Internet_And_Businesses_Online_Demo_Blog_Web_Design_1_0(props);
+    }
+}
+
+const ApplicationFactory = (props: IGlobalContainerProps): any => {
+    return Application(props)
+}
+
+export default ApplicationFactory;
---
+++ Source/4. Pieces/Internet and Businesses Online/Web Development/PieceFactory.tsx
@@ -0,0 +1,24 @@
+﻿//IMPORT APPLICATION COMPONENTS
+import { Global, IGlobalContainerProps, IGlobalHeaderProps, IGlobalBodyProps, IGlobalFooterProps } from '../../../Global';
+import { default as Piece_Self_Improvement_Demo_Todo_Manager_Organizing_1_0 } from '../../../4. Pieces/Self Improvement/Organizing/Piece – Self Improvement – Demo Todo Manager, Organizing – 1_0';
+
+//DECLARE FACTORY ENUM
+enum PieceNameEnum {
+    Piece_Self_Improvement_Demo_Todo_Manager_Organizing_1_0 = "piece – self improvement – demo todo manager, organizing – 1_0"
+}
+
+export { PieceNameEnum }
+
+//DECLARE FACTORY METHOD
+const Piece = (props: IGlobalContainerProps): any => {
+    switch (props.RenderID != undefined && props.RenderID != null && props.RenderID.toLowerCase()) {
+        case PieceNameEnum.Piece_Self_Improvement_Demo_Todo_Manager_Organizing_1_0:
+            return new Piece_Self_Improvement_Demo_Todo_Manager_Organizing_1_0(props);
+    }
+}
+
+const PieceFactory = (props: IGlobalContainerProps): any => {
+    return Piece(props)
+}
+
+export default PieceFactory;
